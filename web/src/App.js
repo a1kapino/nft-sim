f5fc03263fa36486037916e5dfd9bc6067e541e2
a1kapino <alexkapinos@yahoo.com>
2022-02-19 15:38:27 +0200

    minor change in web part

--- web/src/App.js
+++ web/src/App.js
@@ -3,18 +3,17 @@ import Web3 from 'web3';
 import { Abi } from './NftSimContract/NftSim';
 
 function App() {
-  const mintPhoneNumber = async () => {
-    const web3 = new Web3(window.ethereum);
-    await window.ethereum.enable();
-    const contractAddress = "0xFcECFe8a10724eF61B2Df1F0E50D1e576F1432c8";
-    const nftContract = web3.eth.Contract(Abi.abi, contractAddress);
-    await nftContract.methods
-      .mint("0xCD68f6E337d2072C83d5C0063d5BFBc378D8B6BB", 380634886650)
-      .call();
-  };
+ 
   return (
     <div className="App">
-      <button onClick={mintPhoneNumber}>Mint</button>
+      <div>You need to sign this message with your private key</div>
+      <div>randomText</div>
+      <div style="height: 50px"></div>
+      <div>Your ethereum address:</div>
+      <input type="text" />
+      <div>Signed message:</div>
+      <texarea></texarea>
+      <button >Mint</button>
     </div>
   );
 }
